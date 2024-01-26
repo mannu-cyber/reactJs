a37d87e7563375b233bf76ab9639e01660d9ef3c
Abhimanyu sahani <sahanimannu844@gmail.com>
2024-01-26 12:19:03 +0530

    start  learning reactJs

---
+++ src/card.js
@@ -0,0 +1,9 @@
+  function Card(name, age, city){
+    const {} = Card
+    return <div>
+<p>this is the card</p>
+</div>
+
+}
+
+export default Card
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import  ReactDom from "react-dom";
+import Card from "./card"
+const root = ReactDom.createRoot(document.getElementById('root'));
+root.render(<>
+
+<h1>
+
+    this is the first time created the react app
+</h1>
+ <hr/>
+
+
+
+
+</>
+)
