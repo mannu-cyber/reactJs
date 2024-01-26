75ed65d3400070eb9d3c73563eeb80f26978c749
Abhimanyu sahani <sahanimannu844@gmail.com>
2024-01-26 20:17:23 +0530

    learn folder structure

--- src/card.js
+++
@@ -1,9 +0,0 @@
-  function Card(name, age, city){
-    const {} = Card
-    return <div>
-<p>this is the card</p>
-</div>
-
-}
-
-export default Card
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import  ReactDom from "react-dom";
-import Card from "./card"
 const root = ReactDom.createRoot(document.getElementById('root'));
 root.render(<>
 
-<h1>
 
-    this is the first time created the react app
-</h1>
- <hr/>
 
 
 
